f1a7af08a32d6e2c0065951b2f08740960c968aa
Mayeul Deries <mayeul.deries@gmail.com>
2024-10-01 16:17:37 +0200

    add trancription animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { useState, useEffect } from 'react'
 import HomePage from './components/HomePage'
 import FileDisplay from './components/FileDisplay'
 import Header from './components/Header'
+import Information from './components/Information'
+import Transcribing from './components/Transcribing'
 
 function App() {
   const [file, setFile] = useState(null)
   const [audioStream, setAudioStream] = useState(null)
+  const [output, setOutput] = useState(null)
+  const [loading, setLoading] = useState(false)
 
   const isAudioAvailable = file || audioStream
 
@@ -23,7 +27,11 @@ function App() {
     <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header />
-        {isAudioAvailable ? (
+        {output ? (
+          <Information output={output} />
+        ) : loading ? (
+          <Transcribing downloading={loading} />
+        ) : isAudioAvailable ? (
           <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={setAudioStream} />
         ) : (
           <HomePage setFile={setFile} setAudioStream={setAudioStream} />
